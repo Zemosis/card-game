97735d63b096f9047185a95b62d86809645f2dd2
Zemosis <zemosisb@gmail.com>
2026-01-15 14:35:08 -0500

    chore: fixed some comment in playerHand

--- src/components/playerHand.jsx
+++ src/components/playerHand.jsx
@@ -1,6 +1,4 @@
-// ============================================
 // PLAYER HAND COMPONENT - Interactive Hand Display
-// ============================================
 
 import React, { useState, useEffect } from 'react';
 import Card from './Card';
