77c6104ec55401edc68dc3dd014eafd5bf277c25
Zemosis <zemosisb@gmail.com>
2026-01-19 06:07:38 -0500

    update: updated localhost URLs to netlify and render.com URLs

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,9 @@
 import io from 'socket.io-client';
 
-// Connect to backend URL
-export const socket = io.connect("http://localhost:3001");
+// If we are in production (Netlify), use Render URL.
+// If we are developing locally, use localhost:3001.
+const URL = process.env.NODE_ENV === 'production' 
+  ? 'https://card-game-paji.onrender.com' 
+  : 'http://localhost:3001';
+
+export const socket = io.connect(URL);
